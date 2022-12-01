94f834b87543d4ff8257793f6add0df4c9cc756c
Mireia Olivé <mirisfu@gmail.com>
2022-12-01 10:28:16 +0100

    update

--- sprint1_entrega_1.2.js
+++ sprint1_entrega_1.2.js
@@ -25,3 +25,32 @@ console.log(home.dirNom());
 
 //Nivell 3
 //Exercici 1
+//Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta.
+//Invoca-la amb diferents definicions.
+//Les classes abstractes no es poden instanciar
+
+class AbstractClass {
+    constructor(name, age, language) {
+        if (new.target == AbstractClass) {
+            throw new Error("this class cannot be instantiated");
+        }
+        this.name = name;
+        this.age = age;
+        this.language = language;
+    }
+}
+
+const createObject = (name, age, language) => {
+    return Object.create(AbstractClass.prototype, {
+        name: { value: name },
+        age: { value: age },
+        language: { value: language },
+    });
+};
+
+let objOne = createObject("Marta", 20, "german");
+//Error: this class cannot be instantiated
+//let objTwo = new AbstractClass("Tomas", 50, "french");
+
+console.log(objOne);
+//console.log(objTwo);
