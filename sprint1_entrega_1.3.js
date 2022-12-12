13a1cba3978ac4f78d93b09a489e77481670df9f
Mireia Olivé <mirisfu@gmail.com>
2022-12-12 21:24:19 +0100

    update

--- sprint1_entrega_1.3.js
+++ sprint1_entrega_1.3.js
@@ -29,3 +29,49 @@ setTimeout(() => {
 //Exercici 1
 //Donats els objectes employees i salaries, crea una arrow function getEmployee()
 //que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+
+let employees = [
+    {
+        id: 1,
+        name: "Linux Torvalds",
+    },
+    {
+        id: 2,
+        name: "Bill Gates",
+    },
+    {
+        id: 3,
+        name: "Jeff Bezos",
+    },
+];
+
+let salaries = [
+    {
+        id: 1,
+        salary: 4000,
+    },
+    {
+        id: 2,
+        salary: 1000,
+    },
+    {
+        id: 3,
+        salary: 2000,
+    },
+];
+
+const getEmployee = (id) => {
+    const employee = employees.find((employees) => employees.id == id);
+    const promise = new Promise((resolve, reject) => {
+        if (!employee) {
+            reject(`Aquest ${id} no existeix`);
+        } else {
+            resolve(employee);
+        }
+    });
+    return promise;
+};
+
+getEmployee(1)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
