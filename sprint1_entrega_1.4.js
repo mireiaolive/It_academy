c57cb13a8dc07e5e739382a61c423c441f8266cd
Mireia Olivé <mirisfu@gmail.com>
2022-12-13 11:34:41 +0100

    update

--- sprint1_entrega_1.4.js
+++ sprint1_entrega_1.4.js
@@ -2,8 +2,7 @@
 //Exercici 1
 //Crea una funció asíncrona que rebi un id d'empleat/da i imprimeixi per pantalla el nom de
 //l'empleat/da i el seu salari, usant les funcions getEmployee() i getSalary() que has definit a la tasca anterior.
-const getEmployee = require("./sprint1_entrega_1.3.js");
-const getSalary = require("./sprint1_entrega_1.3.js");
+const { getEmployee, getSalary } = require("./sprint1_entrega_1.3.js");
 
 let employees = [
     {
