00631c8ed8a24cca8a5508e5911661eca8baff7f
Mireia Olivé <mirisfu@gmail.com>
2022-12-14 12:12:42 +0100

    update

--- sprint1_entrega_1.4.js
+++ sprint1_entrega_1.4.js
@@ -32,7 +32,7 @@ const employeeInfo = async (id) => {
     }
 };
 
-employeeInfo(1);
+employeeInfo(3);
 
 //Exercici 2
 //Crea una nova funció asíncrona que cridi a una altra que retorni una Promise
@@ -51,3 +51,13 @@ const newFunction = async () => {
 };
 
 newFunction();
+
+//Nivell 2
+//Exercici 1
+//Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
+//Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
+const returnDouble = (num) => {};
+
+//Nivell 3
+//Exercici 1
+//Força i captura tants errors com puguis dels nivells 1 i 2.
