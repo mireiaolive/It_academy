5cabca083ddb138de7bf524044fb480b6528a142
Mireia Olivé <mirisfu@gmail.com>
2022-12-31 13:26:52 +0100

    add code

--- sprint1_entrega_1.4.js
+++ sprint1_entrega_1.4.js
@@ -55,30 +55,48 @@ newFunction();
 //Nivell 2
 //Exercici 1
 //Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
-//Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
 const returnDouble = (num) => {
-    return new Promise((resolve, reject) => {
-        var double = num * 2;
-        if (typeof double === "number") {
-            resolve("Done!");
-        } else {
-            reject("Please use a valid number");
-        }
-        return double;
-    });
-};
-
-console.log(returnDouble(2));
-calculateNumber(2);
-
-const calculateNumber = async () => {
-    try {
-        const result = await returnDouble(num);
-        console.log(result);
-    } catch (err) {
-        console.log(err);
+    if (num === undefined) {
+        num = 0;
+    } else {
+        return new Promise((resolve, reject) => {
+            let isnum = /^\d+$/.test(num);
+            if (isnum) {
+                setTimeout(() => {
+                    resolve(num * 2);
+                }, 2000);
+            } else {
+                reject("Please use a number");
+            }
+        });
     }
 };
+
+module.exports.returnDouble = returnDouble;
+
+//Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
+const getSuma = async (num1, num2, num3) => {
+    if (num1 === null && num2 === null && num3 === null) {
+        console.log("error 1");
+    } else {
+        try {
+            const dob1 = await returnDouble(num1);
+            const dob2 = await returnDouble(num2);
+            const dob3 = await returnDouble(num3);
+            return dob1 + dob2 + dob3;
+        } catch (err) {
+            console.log("error 2");
+        }
+    }
+};
+
+getSuma(9, 8, 7)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+getSuma("6", 5, 4)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 
 //Nivell 3
 //Exercici 1
