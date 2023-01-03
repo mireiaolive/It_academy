d7741d4907ab9b25ef92fe72f4ca7768894ea784
Mireia Olivé <mirisfu@gmail.com>
2023-01-03 12:24:56 +0100

    add code

---
+++ sprint1_entrega_1.5.js
@@ -0,0 +1,16 @@
+//Nivell 1
+//Exercici 1
+//Crea una funció que, en executar-la, escrigui una frase en un fitxer.
+const fs = require("fs");
+
+const data = "This is the new content of file.txt.";
+fs.writeFile("./assets/file.txt", data, (err) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("Data has been written.");
+    }
+});
+
+//Exercici 2
+//Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
