17104dddd269ca9529d8309d54fdda1130da9ded
Mireia Olivé <mirisfu@gmail.com>
2023-01-09 10:20:05 +0100

    code added

--- app/__tests__/sprint1_entrega_1.4.test.js
+++ app/__tests__/sprint1_entrega_1.4.test.js
@@ -1,7 +1,14 @@
-const { newFunction } = require("../sprint1_entrega_1.4");
+const { newFunction, returnDouble } = require("../sprint1_entrega_1.4");
+
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
 
 test("Should return a fn after 2 seconds of the second call.", () => {
     newFunction().then((result) => {
         expect(result).toBe("resolved after 2 seconds");
     });
+});
+
+test("waits 2 second after passed the number twice", () => {
+    expect(setTimeout).toHaveBeenCalledTimes(1);
 });
--- app/sprint1_entrega_1.4.js
+++ app/sprint1_entrega_1.4.js
@@ -17,3 +17,51 @@ const newFunction = async () => {
 newFunction();
 
 module.exports.newFunction = newFunction;
+
+//Nivell 2, Testing
+//N2 E1
+//Crea una funció que retorni el doble del número que li passa com a paràmetre després de 2 segons.
+const returnDouble = (num) => {
+    if (num === null) {
+        num = 0;
+    } else {
+        return new Promise((resolve, reject) => {
+            let isnum = /^\d+$/.test(num);
+            if (isnum) {
+                setTimeout(() => {
+                    resolve(num * 2);
+                }, 2000);
+            } else {
+                reject("Please use a number");
+            }
+        });
+    }
+};
+
+module.exports.returnDouble = returnDouble;
+
+//Crea una altra funció que rebi tres números i calculi la suma dels seus dobles fent servir la funció anterior.
+const getSuma = async (num1, num2, num3) => {
+    if (num1 === null && num2 === null && num3 === null) {
+        console.log("error 1");
+    } else {
+        try {
+            const dob1 = await returnDouble(num1);
+            const dob2 = await returnDouble(num2);
+            const dob3 = await returnDouble(num3);
+            return dob1 + dob2 + dob3;
+        } catch (err) {
+            console.log("error 2");
+        }
+    }
+};
+
+module.exports.getSuma = getSuma;
+
+getSuma(9, 8, 7)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+getSuma("6", 5, 500)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
