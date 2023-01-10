c5442341dc63f0f806b6be84fc778df9d7bc2387
Mireia Olivé <mirisfu@gmail.com>
2023-01-10 19:36:12 +0100

    <message>

--- sprint1_entrega_1.5.js
+++ sprint1_entrega_1.5.js
@@ -9,22 +9,30 @@ const crypto = require("crypto");
 //Exercici 1
 //Crea una funció que, en executar-la, escrigui una frase en un fitxer.
 const data = "This is the new content of file.txt.";
-fs.writeFile("file.txt", data, (err) => {
-    if (err) {
-        throw err;
-    } else {
-        //console.log("Data has been written.");
-    }
-});
+const writeText = () => {
+    fs.writeFile("file.txt", data, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            //console.log("Data has been written.");
+        }
+    });
+};
+
+writeText();
 
 //Exercici 2
 //Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
-fs.readFile("file.txt", (err, data) => {
-    if (err) {
-        throw err;
-    }
-    console.log(data.toString());
-});
+const showText = () => {
+    fs.readFile("file.txt", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        console.log(data.toString());
+    });
+};
+
+showText();
 
 //Exercici 3
 //Crea una funció que comprimeixi el fitxer del nivell 1.
