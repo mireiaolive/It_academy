91163d24ce400eb80fbd8bc1a8857e5fdf591e46
Mireia Olivé <mirisfu@gmail.com>
2023-01-10 10:11:34 +0100

    add changes

--- app/__tests__/sprint1_entrega_1.2.test.js
+++ app/__tests__/sprint1_entrega_1.2.test.js
@@ -3,7 +3,28 @@
 const { Persona, createObject } = require("../sprint1_entrega_1.2");
 jest.mock("../sprint1_entrega_1.2");
 
-test("calls to Persona", () => {
+//to clear mock function after each test:
+beforeEach(() => {
+    Persona.mockClear();
+    createObject.mockClear();
+});
+
+test("call to Persona", () => {
     Persona("Jonathan");
     expect(Persona).toHaveBeenCalledTimes(1);
+});
+
+test("call to dirNom", () => {
+    expect(Persona).not.toHaveBeenCalled();
+    const singer = new Persona("Michael");
+    expect(Persona).toHaveBeenCalledTimes(1);
+
+    singer.dirNom();
+    const callMock = Persona.mock.instances[0];
+    expect(callMock.dirNom).toHaveBeenCalledTimes(1);
+});
+
+test("call to createObject", () => {
+    createObject("Michael");
+    expect(createObject).toHaveBeenCalledTimes(1);
 });
